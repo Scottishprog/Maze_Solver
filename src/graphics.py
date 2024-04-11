51a0cd3444c19d8210e6f6edae912360b10af674
Scottishprog <m_wanamaker_jr@sbcglobal.net>
2024-04-11 14:42:07 -0700

    Intital window class is now working.

--- src/graphics.py
+++ src/graphics.py
@@ -2,6 +2,23 @@ from tkinter import Tk, BOTH, Canvas
 
 
 class Window:
-    def __init__(self, width, height, title):
-        self.width = width
-        self.height = height
+    def __init__(self, width=800, height=600, title='Maze Solver'):
+        self.__root = Tk()
+        self.__root.title(title)
+        self.canvas = Canvas(self.__root, bg='white', width=width, height=height)
+        self.canvas.pack(fill=BOTH, expand=True)
+        self.__is_running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__is_running = True
+        while self.__is_running:
+            self.redraw()
+        print('Window has been closed')
+
+    def close(self):
+        self.__is_running = False
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-
+from graphics import Window
 
 
 def main():
-    pass
+    win = Window(800, 600)
+    win.wait_for_close()
 
 
 main()
