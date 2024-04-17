726cdb27760f3481c64418040889c91dfa4971b9
Scottishprog <m_wanamaker_jr@sbcglobal.net>
2024-04-17 15:13:15 -0700

    Finished and tested the initial implementation of the Maze class.

--- src/main.py
+++ src/main.py
@@ -1,32 +1,14 @@
 from graphics import Window, Point
 from src.cell import Cell
+from src.maze import Maze
 
 
 def main():
     win = Window(800, 600)
 
-    cell1 = Cell(win)
-    cell1.has_right_wall = False
-    cell1.draw(50, 50, 100, 100)
-    cell2 = Cell(win)
-    cell2.has_left_wall = False
-    cell2.has_bottom_wall = False
-    cell2.draw(100, 50, 150, 100)
+    maze = Maze(50, 50, 10, 14, 50, 50, win)
 
-    cell1.draw_move(cell2)
 
-    cell3 = Cell(win)
-    cell3.has_top_wall = False
-    cell3.has_right_wall = False
-    cell3.draw(100,100, 150, 150)
-
-    cell2.draw_move(cell3)
-
-    cell4 = Cell(win)
-    cell4.has_left_wall = False
-    cell4.draw(150, 100, 200, 150)
-
-    cell3.draw_move(cell4, True)
 
     win.wait_for_close()
 
--- src/maze.py
+++ src/maze.py
@@ -1,3 +1,6 @@
+import time
+from cell import Cell
+
 
 class Maze:
     def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
@@ -10,5 +13,27 @@ class Maze:
         self._win = win
         self._create_cell()
 
-    def create_cell(self):
-        pass
+    def _create_cell(self):
+        self._cells = []
+        for i in range(0, self.num_cols):
+            maze_col = []
+            for j in range(0, self.num_rows):
+                maze_col.append(Cell(self._win))
+            self._cells.append(maze_col)
+        print(self._cells)
+
+        for i in range(0, self.num_cols):
+            for j in range(0, self.num_rows):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        _x1 = self.x1 + i * self.cell_size_x
+        _y1 = self.y1 + j * self.cell_size_y
+        _x2 = _x1 + self.cell_size_x
+        _y2 = _y1 + self.cell_size_y
+        cell = self._cells[i][0].draw(_x1, _y1, _x2, _y2)
+        self._animate()
+
+    def _animate(self):
+        self._win.redraw()
+        time.sleep(0.05)
